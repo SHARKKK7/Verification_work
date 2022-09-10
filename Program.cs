f0348b45591126f9e2fe3074a493bde9dbcb7209
sharkkk <kirill.matvieiev.1999@mail.ru>
2022-09-10 20:36:47 +0300

    Добавлен метод для решения задачи

--- Program.cs
+++ Program.cs
@@ -2,6 +2,43 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] inputArrayString = {"iok", "day", "Tuersday",
+         "dah", "Til", "Frih", "Sday", "xih"};
+
+        // Метод подсчета длины элементов <=3 в массиве 
+        string[] UpdateStringArray(string[] inputStringArray)
+        {
+            // Переменная для динамического изменения размера массива
+            int index = 0;
+            // Буферная переменная
+            int j = 0;
+            // Динамический массив в котором буду хранится элементы длиной <=3
+            string[] newStringArray = new string[1] { string.Empty };
+
+            for (int i = 0; i < inputStringArray.Length; i++)
+            {
+                // Проверка условия длины элементов
+                if (inputStringArray[i].Length <= 3)
+                {
+                    Console.WriteLine("Элемент массива {0} [{1}] содержит 3 или меньше символов", i, inputStringArray[i]);
+                    // Здесь index определяет размерность массива
+                    index++;
+                    Array.Resize(ref newStringArray, index);
+                    // Запись элементов в новый массив
+                    while (j < index)
+                    {
+                        newStringArray[j] = inputStringArray[i];
+                        j++;
+                    }
+                }
+            }
+
+            return newStringArray;
+        }
+
+        Console.Clear();
+        //----------------------------------------------------
+        string[] resultStringArray = UpdateStringArray(inputArrayString);
+        //----------------------------------------------------
     }
 }
